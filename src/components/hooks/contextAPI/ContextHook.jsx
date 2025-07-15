e9673d7c593049ee3856ff8a0b5c2d08037b1f62
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-07-16 01:20:48 +0530

    basic styling on contextHook page

--- src/components/hooks/contextAPI/ContextHook.jsx
+++ src/components/hooks/contextAPI/ContextHook.jsx
@@ -7,15 +7,15 @@ const ContextHook = () => {
     <div className='p-15'>
         <div>
             <p className='text-2xl text-white'>This page will simulate a login flow using local storage and also to allow users with Admin rights (logged in user) to access some fake data. </p>
-            <div>
+            {/* <div>
                 <p>Select your theme</p>
-            </div>
+            </div> */}
 
         </div>
 
-        <div >
-            <Button><Link to='/project/login'>Login</Link></Button>
-            <Button><Link to='/project/signup'>Signup</Link></Button>
+        <div className='flex gap-5 mt-5'>
+            <Button className="hover:cursor-pointer"><Link to='/project/login'>Login</Link></Button>
+            <Button className="hover:cursor-pointer"><Link to='/project/signup'>Signup</Link></Button>
         </div>
     </div>
   )
