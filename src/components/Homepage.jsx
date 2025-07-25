a7b116ba5ce7a78c0eba1d329d7483fe1a934c7f
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-07-25 05:52:57 +0530

    Context Intro done

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,7 +6,7 @@ const Homepage = () => {
   return (
     <div className=" p-15 flex flex-col gap-10">
         <h1 className="text-5xl sm:text-6xl md:text-7xl xl:text-8xl font-extrabold text-white ">Welcome to <span className="text-purple-600">Shwetank's </span><span className="text-blue-500">React ⚛️</span> cluster!🛸</h1>
-        <p className="text-xl">This website contains my idea and logic of everything that I have learnt in the past year. This is my best project and this has every <span className="text-white">React Hook.</span></p>
+        <p className="text-xl">This website contains my idea and logic of everything that I have learnt in the past year. This is my best project and this has every <Link target="_blank" to={'https://react.dev/reference/react/hooks'} className="text-white underline italic font-semibold">React Hook.</Link></p>
         <div className="flex gap-5">
           <Button className="text-sm sm:text-xl hover:cursor-pointer" variant="homepage"><Link to={'/projects'}>Click here</Link></Button>
           <Button className="text-sm sm:text-xl hover:cursor-pointer" variant="homepage"><Link to={'/project/admin'}>Click here Admin</Link></Button>
--- src/components/hooks/contextAPI/ContextHook.jsx
+++ src/components/hooks/contextAPI/ContextHook.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
 import { Button } from '@/components/ui/button'
 import { Link } from 'react-router'
 
 const ContextHook = () => {
   return (
     <div className='p-15'>
-        <div>
-            <p className='text-2xl text-white'>This page will simulate a login flow using local storage and also to allow users with Admin rights (logged in user) to access some fake data. </p>
-            {/* <div>
-                <p>Select your theme</p>
-            </div> */}
-
+        <div className='flex flex-col gap-5'>
+          <p className='text-xl sm:text-xl md:text-2xl   text-white'><span className='text-3xl sm:text-5xl md:text-6xl font-bold text-amber-300 '>ContextAPI</span> is used where we need to send data directly to a child instead of passing it through every component as prop. Due to this, the problem of <span className='underline italic font-semibold text-sky-600'>PropDrilling</span> arises and to counter, we use ContextAPI. </p>
+          <p className="text-xl sm:text-xl md:text-2xl text-white">
+            useContext gives us access to a Provider and a Consumer. To make a component a Provider, we use: 
+            <code className="bg-gray-800 text-green-300 px-1 rounded">
+              {`<globalContext.Provider value={{auth, setAuth}} ></globalContext.Provider>`}
+            </code>
+          </p>
+          <p className='text-xl sm:text-xl md:text-2xl  text-white'>This is a project of contextAPI which a protected route, simulating a login flow using local storage and also to allow users with Admin rights (logged in user) to access some fake data. </p>
         </div>
 
         <div className='flex gap-5 mt-5'>
-            <Button className="hover:cursor-pointer"><Link to='/project/login'>Login</Link></Button>
-            <Button className="hover:cursor-pointer"><Link to='/project/signup'>Signup</Link></Button>
+            <Button className="bg-pink-700 hover:bg-pink-400 hover:cursor-pointer"><Link to='/project/login'>Login</Link></Button>
+            <Button className="bg-purple-700 hover:bg-purple-400 hover:cursor-pointer"><Link to='/project/signup'>Signup</Link></Button>
         </div>
     </div>
   )
