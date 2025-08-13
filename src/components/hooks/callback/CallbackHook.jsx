e2ddb00359433bcda869bde5ab1bb8e299a27016
Shwetank <mshwetank@gmail.com>
2025-08-13 15:40:21 +0530

    feat: working on useCallback

--- src/components/hooks/callback/CallbackHook.jsx
+++ src/components/hooks/callback/CallbackHook.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useCallback, useState } from 'react'
 
 const CallbackHook = () => {
+
+  const [count, setCount] = useState(0);
+
+  const cart= ["Shwetank", "swapnil", "Adi", "akshat"];
+
+  
+  const promise1 = new Promise((req,res) => {
+    return function(){
+      if(cart){
+        res("the data is", cart)
+      }
+      else throw new Error("Not found")
+    } 
+  })
+  useEffect(() => {
+    const fetchData = async() => {
+      promise1().then ((data) => {
+        console.log(data)
+      })
+      .catch((err) => err)
+    }
+    fetchData()
+  }, [])
+
+  let add = useCallback(function(){
+    setCount((i) => i+1);
+    console.log("Memoized re-rendered")
+  }, [])
+
   return (
     <div className='p-15'>
-        Will work later
+      <button onClick={add}>Add</button>
+      <>
+      {count}
+      </>
     </div>
   )
 }
