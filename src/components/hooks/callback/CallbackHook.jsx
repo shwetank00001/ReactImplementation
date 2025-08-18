7cd865f633da3ba08498e3c4ae68581edab79f0d
Shwetank <mshwetank@gmail.com>
2025-08-18 12:42:33 +0530

    feat: Setting up like feature

--- src/components/hooks/callback/CallbackHook.jsx
+++ src/components/hooks/callback/CallbackHook.jsx
@@ -1,41 +1,39 @@
 import React, { useEffect, useCallback, useState } from 'react'
+import InstaCard from './InstaCard'
 
 const CallbackHook = () => {
 
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const cart= ["Shwetank", "swapnil", "Adi", "akshat"];
+  // const cart= ["Shwetank", "swapnil", "Adi", "akshat"];
 
   
-  const promise1 = new Promise((req,res) => {
-    return function(){
-      if(cart){
-        res("the data is", cart)
-      }
-      else throw new Error("Not found")
-    } 
-  })
-  useEffect(() => {
-    const fetchData = async() => {
-      promise1().then ((data) => {
-        console.log(data)
-      })
-      .catch((err) => err)
-    }
-    fetchData()
-  }, [])
+  // const promise1 = new Promise((req,res) => {
+  //   return function(){
+  //     if(cart){
+  //       res("the data is", cart)
+  //     }
+  //     else throw new Error("Not found")
+  //   } 
+  // })
+  // useEffect(() => {
+  //   const fetchData = async() => {
+  //     promise1().then ((data) => {
+  //       console.log(data)
+  //     })
+  //     .catch((err) => err)
+  //   }
+  //   fetchData()
+  // }, [])
 
-  let add = useCallback(function(){
-    setCount((i) => i+1);
-    console.log("Memoized re-rendered")
-  }, [])
+  // let add = useCallback(function(){
+  //   setCount((i) => i+1);
+  //   console.log("Memoized re-rendered")
+  // }, [])
 
   return (
     <div className='p-15'>
-      <button onClick={add}>Add</button>
-      <>
-      {count}
-      </>
+      <InstaCard />
     </div>
   )
 }
---
+++ src/components/hooks/callback/InstaCard.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@mui/material';
+
+const InstaCard = () => {
+    const url = 'https://dummyjson.com/posts';
+    const [postData, setPostData]= useState([]);
+
+
+    useEffect(()=> {
+        async function fetchingPosts(){
+            try {
+                const data = await fetch(url);
+                const response = await data.json();
+                setPostData(response.posts);
+
+            } catch (error) {
+                console.log("While fetching posts", error)
+            }
+        }
+        fetchingPosts()
+    }, []);
+    console.log("postData", postData)
+
+    const ele = postData.map(function(item){
+        return (
+            <Card>
+                <CardHeader>
+                    <CardTitle>{item.title}</CardTitle>
+                    <CardDescription>{item.body}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Button onClick={() => increaseLikes(item.id)}>👍🏻 {item.reactions.likes}</Button>
+                    {/* <p>Dislikes: {item.reactions.dislikes}</p> */}
+                </CardContent>
+                <CardFooter>
+                </CardFooter>
+            </Card>
+        )
+    })
+  return (
+    <div className='grid grid-cols-4 gap-5'>
+        {ele}
+    </div>
+  )
+}
+
+export default InstaCard
